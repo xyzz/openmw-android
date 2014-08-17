59d78e309dd79162fd43ee93dff8df24bc853dda
sylar <sylar@debian>
2014-08-17 23:47:29 +0400

    fix with destroy activity

--- src/com/libopenmw/openmw/MainActivity.java
+++ src/com/libopenmw/openmw/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.CheckBox;
 public class MainActivity extends Activity {
 
 	public Context context;
-	public static boolean contols=true;
+	public static boolean contols = true;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,36 +23,34 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main);
 
 		context = this;
-		final CheckBox Box=(CheckBox) findViewById(R.id.checkBox1);
-	
-	
-		
+		final CheckBox Box = (CheckBox) findViewById(R.id.checkBox1);
+
 		Box.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				if(Box.isChecked()){
-					contols=false;
-                }else{
-                	contols=true;
-                }
+				if (Box.isChecked()) {
+					contols = false;
+				} else {
+					contols = true;
+				}
 
 			}
 
 		});
-	
+
 		final Button button = (Button) findViewById(R.id.start);
 		button.setOnClickListener(new View.OnClickListener() {
-			@SuppressLint("InlinedApi") public void onClick(View v) {
+			@SuppressLint("InlinedApi")
+			public void onClick(View v) {
 				Intent intent = new Intent(context, SDLActivity.class);
 
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				context.startActivity(intent);
-			//	finish();
+				finish();
 
 			}
 
 		});
-	
+
 	}
-	
 
 }
