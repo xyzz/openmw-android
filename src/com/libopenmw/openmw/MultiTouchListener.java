b433e24899aa1cd0e468e24ca68ef426e8c57489
root <root@debian>
2014-08-29 12:41:34 +0400

    class for move buttons

---
+++ src/com/libopenmw/openmw/MultiTouchListener.java
@@ -0,0 +1,63 @@
+package com.libopenmw.openmw;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.RelativeLayout;
+
+public class MultiTouchListener implements OnTouchListener
+{
+
+private float mPrevX;
+private float mPrevY;
+
+public ConfigureControls Activity;
+public MultiTouchListener(ConfigureControls configureControls) {
+    Activity = configureControls;
+}
+
+@Override
+public boolean onTouch(View view, MotionEvent event) {
+    float currX,currY;
+    int action = event.getAction();
+    switch (action ) {
+        case MotionEvent.ACTION_DOWN: {
+
+            mPrevX = event.getX();
+            mPrevY = event.getY();
+            break;
+        }
+
+        case MotionEvent.ACTION_MOVE:
+        {
+
+                currX = event.getRawX();
+                currY = event.getRawY();
+
+
+                MarginLayoutParams marginParams = new MarginLayoutParams(view.getLayoutParams());   
+                marginParams.setMargins((int)(currX - mPrevX), (int)(currY - mPrevY),0, 0);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(marginParams);
+                view.setLayoutParams(layoutParams); 
+
+
+            break;
+        }
+
+
+
+        case MotionEvent.ACTION_CANCEL:
+            break;
+
+        case MotionEvent.ACTION_UP:
+
+            break;
+    }
+
+    return true;
+}
+
+}
