f6640c0efaf6d4da6d65e8b588fcaceed5da380a
root <root@debian>
2014-09-07 19:40:14 +0400

    added opacity and size settings buttons

--- src/com/libopenmw/openmw/ConfigureControls.java
+++ src/com/libopenmw/openmw/ConfigureControls.java
@@ -5,11 +5,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ConfigureControls extends Activity {
 
@@ -20,7 +23,16 @@ public class ConfigureControls extends Activity {
 	public ImageButton buttonConsole;
 	public ImageButton buttonChangePerson;
 	public ImageButton buttonWait;
+	public Button buttonSize;
+	public Button buttonSize1;
 	public Button buttonTouch;
+	
+	public Button buttonOpacity;
+	public Button buttonOpacity1;
+	public TextView opacity;
+	public TextView size;
+	public TextView button;
+
 	public ImageButton buttonDiary;
 	public ImageButton buttonPause;
 	public ImageButton buttonLoad;
@@ -32,6 +44,7 @@ public class ConfigureControls extends Activity {
 	public ImageButton buttonMagic;
 	public ImageButton buttonUse;
 	public ImageButton buttonCrouch;
+	
 
 	public SharedPreferences Settings;
 
@@ -56,13 +69,94 @@ public class ConfigureControls extends Activity {
 		CoordinatesAllScreens.height = displaymetrics.heightPixels;
 		CoordinatesAllScreens.width = displaymetrics.widthPixels;
 		final MultiTouchListener touchListener = new MultiTouchListener(this);
+		
+		buttonSize = (Button) findViewById(R.id.buttonsize1);
+		buttonSize1 = (Button) findViewById(R.id.buttonsize2);
+		button=(TextView) findViewById(R.id.textViewTitle);
+		size=(TextView) findViewById(R.id.textViewSize);
+			
+		buttonSize.setTextColor(Color.WHITE);
+		buttonSize.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(40));
+		buttonSize.setLayoutParams(ControlsParams.coordinates(
+				buttonSize, 400, 280, 70, 70));
+		buttonSize.setVisibility(Button.VISIBLE);
+		
+		size.setTextColor(Color.WHITE);
+		size.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(30));
+		size.setLayoutParams(ControlsParams.coordinates(
+				size, 463, 300, 150, 150));
+		size.setVisibility(TextView.VISIBLE);
+		
+		buttonSize1.setTextColor(Color.WHITE);
+		buttonSize1.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(40));
+		buttonSize1.setLayoutParams(ControlsParams.coordinates(
+				buttonSize1, 570, 280, 70, 80));
+		buttonSize1.setVisibility(Button.VISIBLE);
+		
+		buttonOpacity = (Button) findViewById(R.id.buttonopacity1);
+		buttonOpacity1 = (Button) findViewById(R.id.buttonopacity2);
+
+		opacity=(TextView) findViewById(R.id.textViewOpacity);
+			
+		buttonOpacity.setTextColor(Color.WHITE);
+		buttonOpacity.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(40));
+		buttonOpacity.setLayoutParams(ControlsParams.coordinates(
+				buttonOpacity, 400, 355, 70, 70));
+		buttonOpacity.setVisibility(Button.VISIBLE);
+		
+		opacity.setTextColor(Color.WHITE);
+		opacity.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(30));
+		opacity.setLayoutParams(ControlsParams.coordinates(
+				opacity, 463, 368, 200, 150));
+		opacity.setVisibility(TextView.VISIBLE);
+		
+		buttonOpacity1.setTextColor(Color.WHITE);
+		buttonOpacity1.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(40));
+		buttonOpacity1.setLayoutParams(ControlsParams.coordinates(
+				buttonOpacity1, 650, 355, 70, 80));
+		buttonOpacity1.setVisibility(Button.VISIBLE);
+		
+		
+		button.setTextColor(Color.WHITE);
+		button.setTextSize((float) CoordinatesAllScreens.getInstance()
+				.getScaledCoordinateX(30));
+		button.setLayoutParams(ControlsParams.coordinates(
+				button, 450, 220, 200, 150));
+		button.setVisibility(TextView.VISIBLE);
+		
 		joystick = (Controls) findViewById(R.id.joystick);
 		joystick.setOnTouchListener(touchListener);
 
 		joystick.setAlpha((float) 0.5);
 
 		buttonRun = (ImageButton) findViewById(R.id.buttonrun1);
-		buttonRun.setOnTouchListener(touchListener);
+		
+		
+		buttonRun.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+
+					
+					buttonRun.setOnTouchListener(touchListener);
+									
+					return true; 
+				case MotionEvent.ACTION_UP:
+					
+					
+					return true; 
+				}
+				return false;
+			}
+		});
+
 
 		buttonRun.setAlpha((float) 0.5);
 
