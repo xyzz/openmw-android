57f5d4f537f1691662469d08476911d8d3d7bdd9
root <root@debian>
2014-09-10 03:08:58 +0400

    add Android.mk file and changed sources for build freeimage for android

---
+++ jni/Application.mk
@@ -0,0 +1,8 @@
+APP_OPTIM := release
+APP_PLATFORM := android-14
+APP_STL := gnustl_static
+APP_CPPFLAGS += -frtti 
+APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -DANDROID
+APP_ABI := armeabi-v7a
+APP_MODULES := FreeImage
