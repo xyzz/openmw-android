2d3da3037847ca4da447c725dc95e89a034a5c4c
root <root@debian>
2015-04-21 20:22:58 +0400

    save data after every changes in textview

--- src/fragments/TextListener.java
+++ src/fragments/TextListener.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences.Editor;
 import android.text.Editable;
 import android.text.TextWatcher;
 
-
 public class TextListener implements TextWatcher {
 
 	private SharedPreferences Settings;
@@ -19,32 +18,20 @@ public class TextListener implements TextWatcher {
 	private String gameValue;
 	private Activity a;
 
-	public TextListener(Activity a,String data, String path, String value,
-			String sharedprefValue, String gameValue,SharedPreferences Settings) {
+	public TextListener(Activity a, String data, String path, String value,
+			String sharedprefValue, String gameValue, SharedPreferences Settings) {
 		this.data = data;
 		this.path = path;
 		this.value = value;
 		this.sharedprefValue = sharedprefValue;
 		this.gameValue = gameValue;
-		this.Settings=Settings;
-		this.a=a;
+		this.Settings = Settings;
+		this.a = a;
 	}
 
 	@Override
 	public void afterTextChanged(final Editable s) {
 
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					Writer.write(s.toString() + data, s.toString() + path,
-							value);
-
-				} catch (Exception e) {
-
-				}
-
-			}
-		}).start();
 	}
 
 	@Override
@@ -58,8 +45,23 @@ public class TextListener implements TextWatcher {
 		saveToSharedPreferences(sharedprefValue, s.toString());
 		gameValue = s.toString();
 		PreferencesHelper.getPrefValues(a);
+		saveData(s.toString());
 	}
-	
+
+	private void saveData(final String s) {
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					Writer.write(s + data, s.toString() + path, value);
+
+				} catch (Exception e) {
+
+				}
+
+			}
+		}).start();
+	}
+
 	private void saveToSharedPreferences(String value, String buffer) {
 		Editor editor = Settings.edit();
 		editor.putString(value, buffer);
