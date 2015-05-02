bbbbbab3cc0b8ca2cfbacdbc310aa697575f6d5f
root <root@debian>
2015-05-02 09:17:25 +0400

    Migration project  from eclipse to android studio

--- jni/FreeImage/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-APP_OPTIM := release
-APP_PLATFORM := android-14
-APP_STL := gnustl_static
-APP_CPPFLAGS += -frtti 
-APP_CPPFLAGS += -fexceptions
-APP_CPPFLAGS += -DANDROID
-APP_ABI := armeabi-v7a
-APP_MODULES := FreeImage
