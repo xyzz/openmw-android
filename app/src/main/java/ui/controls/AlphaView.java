fd2d4f6af9cb1c81f0583541c162caca1702251a
sandstranger <sandstranger@gmail.com>
2015-07-09 15:16:07 +0400

    enable setalpha method for sdk>=11

--- app/src/main/java/ui/controls/AlphaView.java
+++ app/src/main/java/ui/controls/AlphaView.java
@@ -1,5 +1,6 @@
 package ui.controls;
 
+import android.os.Build;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 
@@ -8,9 +9,12 @@ import android.view.animation.AlphaAnimation;
  */
 public class AlphaView {
     public static void setAlphaForView(View v, float alpha) {
-        AlphaAnimation animation = new AlphaAnimation(alpha, alpha);
-        animation.setDuration(0);
-        animation.setFillAfter(true);
-        v.startAnimation(animation);
+        if (Build.VERSION.SDK_INT < 11) {
+            AlphaAnimation animation = new AlphaAnimation(alpha, alpha);
+            animation.setDuration(0);
+            animation.setFillAfter(true);
+            v.startAnimation(animation);
+        } else
+            v.setAlpha(alpha);
     }
 }
