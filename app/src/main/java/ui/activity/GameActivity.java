f7200bb84942ee452753e40e26ec1905e230b939
sandstranger <sandstranger@gmail.com>
2015-08-29 20:07:57 +0400

    start working on gyroscope controls

--- app/src/main/java/ui/activity/GameActivity.java
+++ app/src/main/java/ui/activity/GameActivity.java
@@ -1,6 +1,7 @@
 package ui.activity;
 
 import java.io.File;
+import java.util.List;
 
 import org.libsdl.app.SDLActivity;
 
@@ -10,20 +11,30 @@ import ui.controls.QuickPanel;
 import ui.controls.ScreenControls;
 import ui.files.CommandlineParser;
 
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Process;
 import android.preference.PreferenceManager;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class GameActivity extends SDLActivity {
+public class GameActivity extends SDLActivity implements SensorEventListener {
 
     public static native void getPathToJni(String path);
+
     public static native void commandLine(int argc, String[] argv);
-   // public static native void saveCurrentTextureCompressionMode (String textureCompressionMode);
+
+    private SensorManager sManager;
+    private boolean useGyroscope = false;
+    // public static native void saveCurrentTextureCompressionMode (String textureCompressionMode);
 
     private boolean hideControls = false;
 
@@ -41,7 +52,7 @@ public class GameActivity extends SDLActivity {
         CommandlineParser commandlineParser = new CommandlineParser(Constants.commandLineData);
         commandlineParser.parseCommandLine();
         commandLine(commandlineParser.getArgc(), commandlineParser.getArgv());
-    //    saveCurrentTextureCompressionMode(Constants.textureCompressionMode);
+        //    saveCurrentTextureCompressionMode(Constants.textureCompressionMode);
         hideControls = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.HIDE_CONTROLS, false);
         getPathToJni(Constants.configsPath);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -52,7 +63,15 @@ public class GameActivity extends SDLActivity {
         panel.showQuickPanel(hideControls);
         if (!hideControls)
             QuickPanel.getInstance().f1.setVisibility(Button.VISIBLE);
+        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        useGyroscope = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.USE_GYROSCOPE, false);
+
+    }
+
+    private boolean isSensorAvailable() {
+        PackageManager PM = this.getPackageManager();
+        return PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
     }
 
     private void deleteVideoFile() {
@@ -69,6 +88,36 @@ public class GameActivity extends SDLActivity {
         Process.killProcess(Process.myPid());
         super.onDestroy();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sManager.registerListener(this, sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onStop() {
+        sManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (useGyroscope && isSensorAvailable()) {
+
+            if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+                return;
+            }
+
+            SDLActivity.onNativeTouch(0, 0,
+                    MotionEvent.ACTION_DOWN, event.values[2], event.values[1], 0);
+        }
+    }
+
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
