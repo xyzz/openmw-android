2b8ddde57d08381b6a5de82851207f58e054a7de
sandstranger <sandstranger@gmail.com>
2015-10-21 00:28:51 +0300

    controls works in main thread

--- app/src/main/java/ui/controls/SdlNativeKeys.java
+++ app/src/main/java/ui/controls/SdlNativeKeys.java
@@ -8,25 +8,12 @@ public class SdlNativeKeys {
 
     public static void keyDown(final int keyCode) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SDLActivity.onNativeKeyDown(keyCode);
-
-            }
-        }).start();
+        SDLActivity.onNativeKeyDown(keyCode);
     }
 
 
     public static void keyUp(final int keyCode) {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SDLActivity.onNativeKeyUp(keyCode);
-
-            }
-        }).start();
+        SDLActivity.onNativeKeyUp(keyCode);
     }
 
     public static void touchDown(final float x, final float y, final int eventAction, MotionEvent event) {
@@ -34,14 +21,9 @@ public class SdlNativeKeys {
         int i = event.getActionIndex();
         final int pointerID = event.getPointerId(i);
         final float pointerCount = event.getPressure(i);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SDLActivity.onNativeTouch(touchDevId, pointerID,
-                        eventAction, x, y, pointerCount);
+        SDLActivity.onNativeTouch(touchDevId, pointerID,
+                eventAction, x, y, pointerCount);
 
-            }
-        }).start();
 
     }
 
