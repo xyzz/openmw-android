72c589472b9660a1f55ff8dfbabac114c752b7c2
sandstranger <sandstranger@gmail.com>
2015-10-20 23:54:25 +0300

    escape key now work

---
+++ app/src/main/java/org/libsdl/app/EscapeKeySimulation.java
@@ -0,0 +1,20 @@
+package org.libsdl.app;
+
+import android.util.Log;
+import android.view.KeyEvent;
+
+/**
+ * Created by sandstranger on 20.10.15.
+ */
+public class EscapeKeySimulation {
+    private static final int KEYCODE_BACK = 4;
+    private static final int KEYCODE_ESCAPE = 27;
+
+    public static void onBackPressed(int keyCode, KeyEvent event) {
+        if (keyCode == KEYCODE_BACK)
+            if (event.getAction() == KeyEvent.ACTION_DOWN)
+                SDLActivity.onNativeKeyDown(KeyEvent.KEYCODE_ESCAPE);
+            else if (event.getAction() == KeyEvent.ACTION_UP)
+                SDLActivity.onNativeKeyUp(KeyEvent.KEYCODE_ESCAPE);
+    }
+}
