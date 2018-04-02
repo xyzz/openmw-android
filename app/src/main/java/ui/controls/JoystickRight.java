3f0e5d5d8e5193d426d55b9e58b4e3011812dbce
Ilya Zhuravlev <whatever@xyz.is>
2018-04-02 00:06:10 -0400

    joystick: make short tap on right joystick do a click

--- app/src/main/java/ui/controls/JoystickRight.java
+++ app/src/main/java/ui/controls/JoystickRight.java
@@ -1,6 +1,7 @@
 package ui.controls;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
@@ -8,7 +9,7 @@ import org.libsdl.app.SDLActivity;
 
 public class JoystickRight extends Joystick {
 
-    private float startX, startY;
+    private float startX, startY, curX, curY;
 
     public JoystickRight(Context context) {
         super(context);
@@ -25,9 +26,8 @@ public class JoystickRight extends Joystick {
     @Override public boolean onTouchEvent(MotionEvent event) {
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_POINTER_DOWN:
-                startX = event.getX();
-                startY = event.getY();
+                startX = curX = event.getX();
+                startY = curY = event.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 float newX = event.getX();
@@ -35,13 +35,26 @@ public class JoystickRight extends Joystick {
 
                 float mouseScalingFactor = 2.0f; // TODO: make configurable
 
-                float movementX = (newX - startX) * mouseScalingFactor;
-                float movementY = (newY - startY) * mouseScalingFactor;
+                float movementX = (newX - curX) * mouseScalingFactor;
+                float movementY = (newY - curY) * mouseScalingFactor;
 
                 SDLActivity.sendRelativeMouseMotion(Math.round(movementX), Math.round(movementY));
 
-                startX = newX;
-                startY = newY;
+                curX = newX;
+                curY = newY;
+                break;
+            case MotionEvent.ACTION_UP:
+                float diffX = event.getX() - startX;
+                float diffY = event.getY() - startY;
+
+                // Simulate a click if finger didn't move much
+                double distance = Math.sqrt(diffX * diffX + diffY * diffY);
+                if (distance < getWidth() / 50) {
+                    SDLActivity.sendMouseButton(1, 1);
+                    final Handler handler = new Handler();
+                    handler.postDelayed(() -> SDLActivity.sendMouseButton(0, 1), 100);
+                }
+
                 break;
         }
 
