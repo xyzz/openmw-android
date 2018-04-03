e3b3432cac5aa48e90a41b9b4c1d0d9ae7aeffc2
Ilya Zhuravlev <whatever@xyz.is>
2018-04-03 00:41:03 -0400

    joystick: remove short/long taps on right joystick

--- app/src/main/java/ui/controls/JoystickRight.java
+++ app/src/main/java/ui/controls/JoystickRight.java
@@ -8,10 +8,9 @@ import android.view.MotionEvent;
 
 import org.libsdl.app.SDLActivity;
 
-public class JoystickRight extends Joystick implements Runnable {
+public class JoystickRight extends Joystick {
 
-    private float startX, startY, curX, curY;
-    private boolean isHolding = false;
+    private float curX, curY;
 
     public JoystickRight(Context context) {
         super(context);
@@ -28,12 +27,8 @@ public class JoystickRight extends Joystick implements Runnable {
     @Override public boolean onTouchEvent(MotionEvent event) {
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-                startX = curX = event.getX();
-                startY = curY = event.getY();
-
-                // Start the hold timer
-                isHolding = false;
-                getHandler().postDelayed(this, 100);
+                curX = event.getX();
+                curY = event.getY();
 
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -50,38 +45,8 @@ public class JoystickRight extends Joystick implements Runnable {
                 curX = newX;
                 curY = newY;
                 break;
-            case MotionEvent.ACTION_UP:
-                float diffX = event.getX() - startX;
-                float diffY = event.getY() - startY;
-
-                getHandler().removeCallbacks(this);
-                if (isHolding) {
-                    SDLActivity.sendMouseButton(0, 1);
-                } else {
-                    // Simulate a short click if finger didn't move much
-                    double distance = Math.sqrt(diffX * diffX + diffY * diffY);
-                    if (distance < getWidth() / 50) {
-                        SDLActivity.sendMouseButton(1, 1);
-                        final Handler handler = new Handler();
-                        handler.postDelayed(() -> SDLActivity.sendMouseButton(0, 1), 100);
-                    }
-                }
-
-                break;
         }
 
         return super.onTouchEvent(event);
-    }
-
-    @Override
-    public void run() {
-        float diffX = curX - startX;
-        float diffY = curY - startY;
-        double distance = Math.sqrt(diffX * diffX + diffY * diffY);
-        if (distance < getWidth() / 50) {
-            isHolding = true;
-            // Press left
-            SDLActivity.sendMouseButton(1, 1);
-        }
     }
 }
