b88c2d1d14be66606124936e9a2213c72b4e1054
Ilya Zhuravlev <whatever@xyz.is>
2019-03-01 00:37:23 -0500

    buildscripts: move the symbols around

--- buildscripts/package-symbols.sh
+++ buildscripts/package-symbols.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-rm -rf tmp
-mkdir -p tmp/symbols
-
-for path in build/*; do
-    arch=$(basename $path)
-    echo "Found symbols for $arch"
-    mkdir -p tmp/symbols/$arch
-    cp build/$arch/symbols/* tmp/symbols/$arch/
-done
-
-cd tmp
+rm -f symbols.7z
 7z a symbols.7z symbols
-cd ..
-
-mv tmp/symbols.7z .
