fc5b873eaa8f00c267c135c9dd9ccc22afc479ab
Ilya Zhuravlev <whatever@xyz.is>
2019-04-24 13:36:37 -0400

    Constants: remove unused

--- app/src/main/java/constants/Constants.java
+++ app/src/main/java/constants/Constants.java
@@ -1,19 +1,6 @@
 package constants;
 
 public class Constants {
-
-	public static String textureCompressionMode ="";
 	public static final String APP_PREFERENCES = "settings";
 	public static final String HIDE_CONTROLS = "hidecontrols";
-
-	public static final String DATA_PATH = "data_path";
-	public static final String LANGUAGE = "encoding";
-	public static final String SUBTITLES = "subtitiles";
-	public static final String MIPMAPPING = "mipmapping";
-	public static final String COMMAND_LINE = "commandline";
-	public static final String TOUCH_SENSITIVITY= "camera sensitivity";
-	public static final String CAMERA_MULTIPLISER = "camera y multiplier";
-	public static String commandLineData = "";
-
-	public static String APPLICATION_DATA_STORAGE_PATH ="";
 }
