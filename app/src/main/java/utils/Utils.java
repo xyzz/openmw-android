7deef6c0bdf3b1893c2577248bccc099ad6901f7
Ilya Zhuravlev <whatever@xyz.is>
2019-04-24 13:35:54 -0400

    Utils: remove unused

--- app/src/main/java/utils/Utils.java
+++ app/src/main/java/utils/Utils.java
@@ -3,24 +3,11 @@ package utils;
 import android.app.Activity;
 import android.view.View;
 
-import java.io.File;
-
 /**
  * Created by sandstranger on 15.01.17.
  */
 
 public class Utils {
-    public static void deleteFile(String path) {
-        File file = new File(path);
-        if (file.exists()) {
-            file.delete();
-        }
-    }
-
-    public static boolean fileExists(String path) {
-        return new File(path).exists();
-    }
-
     public static void hideAndroidControls(Activity activity) {
         activity.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
