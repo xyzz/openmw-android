5608fbc6cc24fc9f39cfed78d8f68aab2159c150
Ilya Zhuravlev <whatever@xyz.is>
2019-04-29 11:31:55 -0400

    full-build: fix parallel build again

--- buildscripts/full-build.sh
+++ buildscripts/full-build.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-./clean.sh
+./clean.sh --all
+
+# ARCH=arm below is a hack for our Great Build System
+ARCH=arm ./include/download-ndk.sh
+ARCH=arm ./include/setup-ndk.sh
 
 ./build.sh --arch arm --lto &
+PID0=$!
 
 ./build.sh --arch arm64 --lto --no-resources &
 PID1=$!
@@ -15,4 +20,4 @@ PID2=$!
 ./build.sh --arch x86 --lto --no-resources &
 PID3=$!
 
-wait $PID1 $PID2 $PID3
+wait $PID0 $PID1 $PID2 $PID3
