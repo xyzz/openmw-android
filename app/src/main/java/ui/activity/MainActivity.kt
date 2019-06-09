a222a8ece624093fa660c97a7be6d11d1ff3418f
Ilya Zhuravlev <whatever@xyz.is>
2019-06-09 11:53:39 -0400

    Include https://gitlab.com/OpenMW/openmw/merge_requests/119

--- app/src/main/java/ui/activity/MainActivity.kt
+++ app/src/main/java/ui/activity/MainActivity.kt
@@ -400,13 +400,6 @@ class MainActivity : AppCompatActivity() {
 
                 file.Writer.write(Constants.SETTINGS_DEFAULT_CFG, "allow capsule shape", prefs!!.getString("pref_allowCapsuleShape", "true")!!)
 
-                file.Writer.write(Constants.SETTINGS_DEFAULT_CFG,
-                    "allow unsafe optimizations",
-                    prefs.getString("pref_unsafe_state_graph",
-                                    getString(R.string.pref_unsafe_state_graph_default))!!)
-
-                file.Writer.write(Constants.SETTINGS_DEFAULT_CFG, "preload enabled", prefs!!.getString("pref_preload", "false")!!)
-
                 runOnUiThread {
                     obtainFixedScreenResolution()
                     dialog.hide()
