19f67a8d610991c5eb3ab4fdcf601f085c5631db
Ilya Zhuravlev <whatever@xyz.is>
2020-03-29 14:35:03 -0400

    Force landscape mode in SDL

--- buildscripts/patches/openmw/android_main.cpp
+++ buildscripts/patches/openmw/android_main.cpp
@@ -60,6 +60,7 @@ extern "C" int Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cl
     SDL_GameControllerAddMapping("5669727475616c000000000000000000,Virtual,a:b0,b:b1,back:b15,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b16,leftshoulder:b6,leftstick:b13,lefttrigger:a5,leftx:a0,lefty:a1,rightshoulder:b7,rightstick:b14,righttrigger:a4,rightx:a2,righty:a3,start:b11,x:b3,y:b4");
 
     SDL_SetHint(SDL_HINT_ANDROID_BLOCK_ON_PAUSE, "0");
+    SDL_SetHint(SDL_HINT_ORIENTATIONS, "LandscapeLeft LandscapeRight");
 
     return 0;
 }
